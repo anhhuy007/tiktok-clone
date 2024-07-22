f3f6de2ddaf06a6e4e6bd191e53c72dca8ca38a2
Nguyen Phuc Huy <124686333+HuyNguyen25@users.noreply.github.com>
2024-07-22 10:10:24 -0400

    feat: add theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/presentation/home/home_page/home_page.dart';
+import 'package:tiktok_clone/theme/theme_helper.dart';
 
 import 'core/utils/size_utils.dart';
 import 'core/utils/size_utils.dart';
@@ -17,7 +18,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ProviderScope(
       child: Sizer(builder: (context, orientation, deviceType) {
-        return const MaterialApp(
+        return MaterialApp(
+            theme: ThemeData(
+              colorScheme: ColorSchemes.primaryColorScheme,
+              textTheme: TextTheme(
+                  headlineSmall: CustomTextStyles.headlineSmall,
+                  labelMedium: CustomTextStyles.labelMedium,
+                  titleLarge: CustomTextStyles.titleLarge,
+                  titleMedium: CustomTextStyles.titleMedium,
+                  titleSmall: CustomTextStyles.titleSmall
+              ),
+              useMaterial3: true,
+            ),
           title: 'Flutter Demo',
           home: HomeScreen()
         );
