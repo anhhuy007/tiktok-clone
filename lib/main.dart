a562ea2db6e15627559f31fd03dda2c50e3907f1
anhhuy007 <imanhhuy007@gmail.com>
2024-09-20 15:06:25 +0700

    feat: add back button listener (main-not archived)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:double_tap_to_exit/double_tap_to_exit.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:tiktok_clone/core/utils/navigator_services.dart';
 import 'package:tiktok_clone/presentation/authentication/features/login/login_page.dart';
 import 'package:tiktok_clone/presentation/authentication/notifiers/auth_notifier.dart';
@@ -16,16 +19,47 @@ Future<void> main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends HookConsumerWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  Future<bool> _onWillPop(BuildContext context) async {
+    return await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure?'),
+        content: const Text('Do you want to exit the app?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('No'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: const Text('Yes'),
+          ),
+        ],
+      ),
+    ) ?? false;
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ref.watch(authProvider.notifier).loadAuthState();
     final authState = ref.watch(authProvider);
-    return Sizer(
-      builder: (context, orientation, deviceType) {
-        return MaterialApp(
+
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        if (didPop) return;
+        final navigator = Navigator.of(context);
+        final shouldPop = await _onWillPop(context);
+        if (shouldPop) {
+          navigator.pop();
+        }
+      },
+      child: Sizer(
+        builder: (context, orientation, deviceType) {
+          return MaterialApp(
             navigatorKey: NavigatorService.navigatorKey,
             theme: ThemeData(
               colorScheme: ColorSchemes.primaryColorScheme,
@@ -34,7 +68,8 @@ class MyApp extends HookConsumerWidget {
                   labelMedium: CustomTextStyles.labelMedium,
                   titleLarge: CustomTextStyles.titleLarge,
                   titleMedium: CustomTextStyles.titleMedium,
-                  titleSmall: CustomTextStyles.titleSmall),
+                  titleSmall: CustomTextStyles.titleSmall
+              ),
               useMaterial3: true,
             ),
             title: 'Flutter Demo',
@@ -42,9 +77,10 @@ class MyApp extends HookConsumerWidget {
             routes: AppRoutes.routes,
             home: authState.isAuthenticated
                 ? const HomePageContainer()
-                : const LogInPage()
-        );
-      },
+                : const LogInPage(),
+          );
+        },
+      ),
     );
   }
 }
